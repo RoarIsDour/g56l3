c3b9838b443e46aa6e06cbb608c3183696c39c3a
Dima Rusanov <roarisdour@gmail.com>
2017-08-22 17:16:11 +0300

    Dima

--- G56L3/ViewController.swift
+++ G56L3/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        sayHello()
+
+
     }
 
-
+    func sayHello() {
+        print("Hello")
+    }
 }
 
